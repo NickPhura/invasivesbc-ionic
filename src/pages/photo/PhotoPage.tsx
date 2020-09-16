09d53d8c8ce6fbd169bd0d84fc15b1a989f9f9ab
Nick Phura <nickphura@gmail.com>
2020-09-16 10:19:38 -0700

    Update photo page css

--- src/pages/photo/PhotoPage.tsx
+++ src/pages/photo/PhotoPage.tsx
@@ -1,30 +1,19 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonFabList } from '@ionic/react';
-import React from 'react';
-import PhotoContainer from '../../components/photo/PhotoContainer';
-import './PhotoPage.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFabList,
+} from "@ionic/react";
+import React from "react";
+import PhotoContainer from "../../components/photo/PhotoContainer";
+import "./PhotoPage.css";
 
 const PhotoPage: React.FC = (props: any) => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{props.name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{props.name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <PhotoContainer />
-      </IonContent>
-    </>
-  );
+  return <PhotoContainer />;
 };
 
 export default PhotoPage;
