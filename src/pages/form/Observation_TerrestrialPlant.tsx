8ba91dab9d34b795a7af13031261539ee716a8a0
Nick Phura <nickphura@gmail.com>
2020-09-16 11:57:46 -0700

    update uischema

--- src/pages/form/Observation_TerrestrialPlant.tsx
+++ src/pages/form/Observation_TerrestrialPlant.tsx
@@ -296,157 +296,60 @@ const schema = {
 };
 
 const uiSchema = {
-  observation_id: {
-  },
-  workflow_id: {
-  },
-  observation_date: {
-  },
-  observation_time: {
-  },
-  observer_first_name: {
-  },
-  observer_last_name: {
-  },
-  species_id: {
-  },
-  observation_type: {
-  },
-  species_agency_code: {
-  },
-  jurisdiction_code: {
-  },
-  negative_obs_ind: {
-  },
+  observation_id: {},
+  workflow_id: {},
+  observation_date: {},
+  observation_time: {},
+  observer_first_name: {},
+  observer_last_name: {},
+  species_id: {},
+  observation_type: {},
+  species_agency_code: {},
+  jurisdiction_code: {},
+  negative_obs_ind: {},
   subType: {
-    oneOf: [
-      {
-        $ref: "#/definitions/Observation_PlantTerrestial",
-      },
-      {
-        $ref: "#/definitions/Observation_AnimalTerrestrial",
-      },
-      {
-        $ref: "#/definitions/Observation_AnimalAquatic",
-      },
-    ],
-  },
-},
-definitions: {
-  Observation_PlantTerrestial: {
-    properties: {
-      species_density_code: {
-      },
-      species_distribution_code: {
-      },
-      soil_texture_code: {
-      },
-      specific_use_code: {
-      },
-      slope_code: {
-      },
-      aspect_code: {
-      },
-      proposed_action_code: {
-      },
-      flowering: {
-      },
-      plant_life_stage: {
-      },
-      plant_health: {
-      },
-      plant_seed_stage: {
-      },
-      sample_identifier: {
-      },
-      range_unit_number: {
-      },
-      general_comment: {
-      },
-      access_description: {
-      },
-      primary_file_id: {
-      },
-      secondary_file_id: {
-      },
-      sample_taken: {
-      },
-      sample_number: {
-      },
-      aquatic_obs_ind: {
-      },
-      legacy_site_ind: {
-      },
-      early_detection_rapid_resp_ind: {
-      },
-      research_detection_ind: {
-      },
-      well_ind: {
-      },
-      special_care_ind: {
-      },
-      biological_ind: {
-      },
-      Photo_Indicator: {
-      },
-      Bec_Zone: {
-      },
-      RISO: {
-      },
-      IPMA: {
-      },
-      Ownership: {
-      },
-      Regional_District: {
-      },
-      FLNRO_District: {
-      },
-      MOTI_District: {
-      },
-      MOE_Region: {
-      },
+    Number_of_Individuals_observed: {},
+    Life_Stage: {
+      "ui:widget": "textarea",
     },
+    Behaviour: {},
+    sample_taken: {},
+    sample_number: {},
+    general_comment: {},
+    access_description: {},
+    species_density_code: {},
+    species_distribution_code: {},
+    soil_texture_code: {},
+    specific_use_code: {},
+    slope_code: {},
+    aspect_code: {},
+    proposed_action_code: {},
+    flowering: {},
+    plant_life_stage: {},
+    plant_health: {},
+    plant_seed_stage: {},
+    sample_identifier: {},
+    range_unit_number: {},
+    primary_file_id: {},
+    secondary_file_id: {},
+    aquatic_obs_ind: {},
+    legacy_site_ind: {},
+    early_detection_rapid_resp_ind: {},
+    research_detection_ind: {},
+    well_ind: {},
+    special_care_ind: {},
+    biological_ind: {},
+    Photo_Indicator: {},
+    Bec_Zone: {},
+    RISO: {},
+    IPMA: {},
+    Ownership: {},
+    Regional_District: {},
+    FLNRO_District: {},
+    MOTI_District: {},
+    MOE_Region: {},
   },
-  Observation_AnimalTerrestrial: {
-    properties: {
-      Number_of_Individuals_observed: {
-      },
-      Life_Stage: {
-      },
-      Behaviour: {
-      },
-      sample_taken: {
-      },
-      sample_number: {
-      },
-      general_comment: {
-      },
-      access_description: {
-      },
-    },
-  },
-  Observation_AnimalAquatic: {
-    properties: {
-      Number_of_Individuals_observed: {
-      },
-      Life_Stage: {
-      },
-      Behaviour: {
-      },
-      sample_taken: {
-      },
-      sample_number: {
-      },
-      general_comment: {
-      },
-      access_description: {
-      },
-    },
-  },
-}
-
-
-
+};
 
 const Observation_TerrestrialPlant: React.FC = (props: any) => {
   return (
