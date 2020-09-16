104abb35090d806cec7883b96581f2402c9daa10
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2020-09-15 23:37:43 -0700

    Trash button implemented to remove all photos from the state, storage and filesystem

--- src/components/photo/PhotoContainer.tsx
+++ src/components/photo/PhotoContainer.tsx
@@ -1,25 +1,49 @@
 import React, { Component } from "react";
 import "./PhotoContainer.css";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonFabList } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+  IonCol, IonImg, IonActionSheet
+} from '@ionic/react';
+import { usePhotoGallery } from '../../hooks/usePhotoGallery';
+import { attachProps } from '@ionic/react/dist/types/components/utils';
 
-class PhotoContainer extends Component {
-  constructor(props: any) {
-    super(props);
+const PhotoContainer: React.FC = (props: any) => {
 
-    console.log("Photo Constructor!");
-  }
+  const { photos, takePhoto, deletePhotos } = usePhotoGallery();
 
-  async componentDidMount() {
-    console.log("Photo componentDidMount!");
-    // On init setup
-  }
+  return (
+    <div>
+      <p>Placeholder if needed</p>
+      <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.base64 ?? photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center">
+          <IonRow>
+            <IonCol>
+              <IonFabButton onClick={() => takePhoto()}>
+                <IonIcon icon={camera}></IonIcon>
+              </IonFabButton>
 
-  render = () => {
-    return (
-      <div>
-        <p>Placeholder if needed</p>
-      </div>
-    );
-  };
-}
+            </IonCol>
+            <IonCol>
+              <IonFabButton onClick={() => deletePhotos()}>
+                <IonIcon icon={trash}></IonIcon>
+              </IonFabButton>
+            </IonCol>
+          </IonRow>
+        </IonFab>
+      </IonContent>
+    </div>
+  );
+};
 
 export default PhotoContainer;
